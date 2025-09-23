40ef4228838c792e32486758d55b35a386cee60e
jok4lv <joaquinalvarez1@hotmail.com>
2025-09-23 11:50:18 -0300

    Creando challenge final app.js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+function agregarAmigo() {
+  const input = document.getElementById("amigo");
+  const nombre = input.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  amigos.push(nombre);
+  input.value = "";
+
+  mostrarLista ();
+}
+function mostrarLista() {
+  const lista = document.getElementById("listaAmigos");
+
+  lista.innerHTML = "";
+  
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }  
+}
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("No hay amigos en la lista. Agrega al menos uno antes de sortear.");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+  const seleccionado = amigos[indiceAleatorio];
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>🎉 El amigo secreto es: <strong>${seleccionado}</strong> 🎉</li>`;
+}
